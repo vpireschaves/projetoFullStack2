0754c78a32432f405045903539b65ba44814f152
Vinicius Chaves <106564445+vpireschaves@users.noreply.github.com>
2024-03-06 20:00:50 -0300

    criação do método autenticar na loginModel

--- models/loginModel.js
+++ models/loginModel.js
@@ -1,3 +1,7 @@
+import Database from "../db/database.js";
+
+const banco = new Database();
+
 export default class LoginModel {
 
     #email;
@@ -22,6 +26,17 @@ export default class LoginModel {
     constructor (email, senha) {
         this.#email = email,
         this.#senha = senha
+    }
+
+    async autenticar(){
+
+        let sql = "select usu_id from tb_usuario where usu_email = ? and usu_senha = ?";
+
+        let valores = [this.#email, this.#senha];
+
+        let rows = await banco.ExecutaComando(sql, valores);
+
+        return rows.length > 0;
     }
 
     toJSON() {
