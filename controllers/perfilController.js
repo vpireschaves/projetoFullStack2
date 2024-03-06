35638a6126e747a8d40762e2c8f57d7d851c2c4d
Vinicius Chaves <106564445+vpireschaves@users.noreply.github.com>
2024-03-06 20:05:45 -0300

    criação da função listar na PerfilController

--- controllers/perfilController.js
+++ controllers/perfilController.js
@@ -1,12 +1,18 @@
+import PerfilModel from "../models/perfilModel.js"
+
+
 export default class PerfilController {
 
-    listar(req, res) {
-        res.status(200).json()[
-            {
-                id: 1,
-                descricao: 'Administrador'
-            }
-        ]
-    }
+    async listar(req, res) {
 
+        try {
+            let perfil = new PerfilModel();
+            let listaPerfil = await perfil.listar();
+
+            res.status(200).json(listaPerfil);
+            
+        } catch (error) {
+            res.status(500).json({msg: "Erro inesperado! Entre em contato com o nosso suporte técnico!", detalhes: error.message})
+        }
+    }
 }
