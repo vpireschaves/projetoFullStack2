49fa48caebf1ad41fa98e7f9b58416d612280fc0
Vinicius Chaves <106564445+vpireschaves@users.noreply.github.com>
2024-03-12 22:10:25 -0300

    associação da imovelRoute no server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import usuarioRoute from './routes/usuariosRoute.js'; 
 import perfilRoute from './routes/perfilRoute.js';
 import loginRoute from './routes/loginRoute.js';
+import imovelRoute from './routes/imovelRoute.js';
 
 import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
@@ -19,6 +20,7 @@ app.use('/documentacao', swaggerUi.serve, swaggerUi.setup(outputJson));
 app.use('/usuarios', usuarioRoute);
 app.use('/perfil', perfilRoute);
 app.use('/login', loginRoute);
+app.use('/imovel', imovelRoute);
 
 app.listen(5000, function () {
     console.log("backend em execução");
