65886b236d698d14e9e5171476e3be049e65a8c2
Vinicius Chaves <106564445+vpireschaves@users.noreply.github.com>
2024-03-13 19:51:30 -0300

    criação da função obter na imovelController

--- controllers/imovelController.js
+++ controllers/imovelController.js
@@ -11,7 +11,25 @@ export default class ImovelController {
             res.status(200).json(lista);
         }
         catch(ex){
-            res.status(500).json({msg: "Erro inesperado! Entre em contato com o nosso suporte técnico!", detalhes: ex.message});
+            res.status(500).json({msg: "Erro inesperado! Entre em contato com o nosso suporte técnico!"});
+        }
+    }
+
+    async obter (req, res) {
+        try {
+            let { id } = req.params;
+            let imovel = new ImovelModel();
+            let imovelEncontrado = await imovel.obter(id);
+            
+            if(imovelEncontrado != null){
+                res.status(200).json(imovelEncontrado);
+            }
+            else {
+                res.status(404).json({msg: `Imóvel com o id ${id} não encontrado!`});
+            }
+        }
+        catch(ex){
+            res.status(500).json({msg: "Erro inesperado! Entre em contato com o nosso suporte técnico!"});
         }
     }
 }
