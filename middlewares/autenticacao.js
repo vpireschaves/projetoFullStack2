994f1fccc48c968698068961f51958a0c3b5737f
Vinicius Chaves <106564445+vpireschaves@users.noreply.github.com>
2024-03-13 21:48:51 -0300

    atualização da autenticacao para gerar JWT

--- middlewares/autenticacao.js
+++ middlewares/autenticacao.js
@@ -1,3 +1,8 @@
+import jwt from 'jsonwebtoken';
+
+//segredo utilizado para o JWT - apenas de teste
+const JWT_SEGREDO = "M3H4CK34R4M";
+
 export default class Autenticacao {
 
     validar(req, res, next){
@@ -9,5 +14,9 @@ export default class Autenticacao {
             res.status(401).json({msg: "Usuário não autorizado"});
         }
 
+    }
+
+    gerarToken(usuario){
+        return jwt.sign(JSON.stringify(usuario.toJSON()), JWT_SEGREDO);
     }
 }
