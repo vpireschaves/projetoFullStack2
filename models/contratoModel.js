5077fe7492248358bf9706ee6f4da9a371e25fd5
Vinícius Pires Chaves <106564445+vpireschaves@users.noreply.github.com>
2024-03-20 21:07:42 -0300

    preenchimento da contratomodel e criação do método gravar

--- models/contratoModel.js
+++ models/contratoModel.js
@@ -2,4 +2,54 @@
 
 export default class ContratoModel {
     
+    #contratoId;
+    #imovel;
+    #usuario;
+
+    get contratoId() {
+        return this.#contratoId;
+    }
+
+    get imovel() {
+        return this.#imovel;
+    }
+
+    get usuario() {
+        return this.#usuario;
+    }
+
+    set contratoId(contratoId) {
+        this.#contratoId = contratoId;
+    }
+
+    set imovel(imovel) {
+        this.#imovel = imovel;
+    }
+
+    set usuario(usuario) {
+        this.#usuario = usuario;
+    }
+
+    constructor(contratoId, imovel, usuario) {
+        this.#contratoId = contratoId;
+        this.#imovel = imovel;
+        this.#usuario = usuario;
+    }
+
+    toJSON() {
+        return {
+            "contratoId": this.#contratoId,
+            "imovel": this.#imovel.toJSON(),
+            "usuario": this.#usuario.toJSON()
+        }
+    }
+
+    async gravar(){
+        let sql = "INSERT INTO tb_contrato (imv_id, usu_id) VALUES (?, ?)";
+        let valores = [this.#imovel.imovelId, this.#usuario.usuId];
+    
+        let result = await banco.ExecutaComandoNonQuery(sql, valores);
+
+        return result;
+    }
 }
