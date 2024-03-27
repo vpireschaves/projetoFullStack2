c0d9cbd898d027aadaa6c6cd0b73a6ee73859cc5
Vinicius Chaves <106564445+vpireschaves@users.noreply.github.com>
2024-03-26 21:16:47 -0300

    ajuste da locacaoController

--- controllers/locacaoController.js
+++ controllers/locacaoController.js
@@ -6,56 +6,64 @@ import UsuarioModel from "../models/usuarioModel.js";
 export default class LocacaoController {
 
     async locar (req, res) {
-        
-        if (req.body) {
-            let { imovelId } = req.body;
 
-            if (imovelId) {
+        try {
 
-                let imovel = new ImovelModel();
-                imovel = await imovel.obter(imovelId);
-
-                if (imovel.imovelDisponivel == 'S') {
-                    let usuarioId = req.usuarioLogado.usuId;
-                    let contrato = new ContratoModel(0, new ImovelModel(imovelId), new UsuarioModel(usuarioId));
-
-                    contrato.contratoId = await contrato.gravar();
-
-                    //gerar um ano de aluguel para o contrato
-                    for (let i = 0; i < 12; i++) {
-                        let aluguel = new AluguelModel();
-                        aluguel.aluguelValor = imovel.imovelValor;
-                        aluguel.aluguelPago = 'N';
-                        aluguel.contrato = contrato;
-
-                        let dataVencimento = new Date();
-                        dataVencimento.setMonth(dataVencimento.getMonth() + 1);
-
-                        let mes = dataVencimento.getMonth() + 1;
-                        aluguel.aluguelMes = mes;
-                        aluguel.aluguelVencimento = dataVencimento;
-
-                        aluguel.gravar();
-
+            if (req.body) {
+                let { imovelId } = req.body;
+    
+                if (imovelId) {
+    
+                    let imovel = new ImovelModel();
+                    imovel = await imovel.obter(imovelId);
+    
+                    if (imovel.imovelDisponivel == 'S') {
+                        let usuarioId = req.usuarioLogado.usuId;
+                        let contrato = new ContratoModel(0, new ImovelModel(imovelId), new UsuarioModel(usuarioId));
+    
+                        contrato.contratoId = await contrato.gravar();
+    
+                        //gerar um ano de aluguel para o contrato
+                        for (let i = 0; i < 12; i++) {
+                            let aluguel = new AluguelModel();
+                            aluguel.aluguelValor = imovel.imovelValor;
+                            aluguel.aluguelPago = 'N';
+                            aluguel.contrato = contrato;
+    
+                            let dataVencimento = new Date();
+                            dataVencimento.setMonth(dataVencimento.getMonth() + 1);
+    
+                            let mes = dataVencimento.getMonth() + 1;
+                            aluguel.aluguelMes = mes;
+                            aluguel.aluguelVencimento = dataVencimento;
+    
+                            aluguel.gravar();
+                        }
+    
+                        imovel.imovelDisponivel = "N";
+                        imovel.gravar();
+    
+                        res.status(200).json({ msg: "Imóvel locado com sucesso!" });
                     }
-
-                    res.status(200).json({ msg: "Contrato gerado com sucesso!" });
+                    else{
+                        res.status(400).json({ msg: "Imóvel indisponível para locação!" });
+                    }
+    
+                    
+    
                 }
-                else{
-                    res.status(400).json({ msg: "Imóvel indisponível para locação!" });
+                else {
+                    res.status(400).json({ msg: "O corpo da requisição não possui o id do imóvel." });
                 }
-
-                
-
+    
+    
             }
             else {
-                res.status(400).json({ msg: "O corpo da requisição não possui o id do imóvel." });
+                res.status(400).json({ msg: "Requisição inválida!" });
             }
 
-
-        }
-        else {
-            res.status(400).json({ msg: "Requisição inválida!" });
+        } catch (error) {
+            res.status(500).json({msg: "Erro interno de servidor!"});
         }
 
     }
